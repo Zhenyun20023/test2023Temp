96215aeb3479d59a779cd6f4759e3d2050787689
zhenyun <test@gmail.com>
2023-03-15 09:42:12 -0700

    adding new config

--- cmd.sh
+++ cmd.sh
@@ -2,7 +2,7 @@
 # has to be STP Pinot
 mvn clean install -DskipTests -Pbin-dist -Dcheckstyle.skip  -Denforcer.skip=true -Dlicense.skip=true -T
 jdk 11; .m2; 
-cd /Users/zhenyunzhuang/workspace/startree-pinot/startree-distribution/target/startree-distribution-pkg
+cd /Users/zhenyunzhuang/workspace/startree-pinot/startree-distribution/target/startree-pinot-0.13.0-SNAPSHOT-bin/startree-pinot-0.13.0-SNAPSHOT-bin
 
 #start each component one by one. 
 #zookeeper
@@ -11,27 +11,48 @@ export JAVA_OPTS="-Xms1G -Xmx2G -XX:+UseG1GC -XX:MaxGCPauseMillis=200 -Xloggc:gc
 
 #controller
 export JAVA_OPTS="-Xms1G -Xmx3G -XX:+UseG1GC -XX:MaxGCPauseMillis=200 -Xloggc:gc-pinot-controller.log"
-./bin/pinot-admin.sh StartController -zkAddress localhost:2191  -controllerPort 9000
+./bin/pinot-admin.sh StartController -zkAddress localhost:2191  -controllerPort 9000 -configOverride pinot.controller.startable.class=ai.startree.service.startable.pinot.controller.StarTreePinotControllerStarter
 
 #Broker
 export JAVA_OPTS="-Xms1G -Xmx3G -XX:+UseG1GC -XX:MaxGCPauseMillis=200 -Xloggc:gc-pinot-broker.log"
 ./bin/pinot-admin.sh StartBroker  -zkAddress localhost:2191 
 
+
 #Server 
 export JAVA_OPTS="-Xms1G -Xmx4G -XX:+UseG1GC -XX:MaxGCPauseMillis=200 -Xloggc:gc-pinot-server.log"
-./bin/pinot-admin.sh StartServer -zkAddress localhost:2191
+./bin/pinot-admin.sh StartServer -zkAddress localhost:2191  -configOverride  pinot.server.startable.class=ai.startree.service.startable.pinot.server.StarTreePinotServerStarter
+
 
 #start Kafka 
 ./bin/pinot-admin.sh  StartKafka -zkAddress=localhost:2191/kafka -port 19092
 
 # create schema and table config; 
 # on UI;  
+  "upsertConfig": {
+    "mode": "FULL", 
+    "metadataManagerClass": "ai.startree.pinot.upsert.rocksdb.RocksDBTableUpsertMetadataManager"
+  }
+  "routing": {
+    "segmentPrunerTypes": ["partition"],
+    "instanceSelectorType": "strictReplicaGroup"
+  },
+
+#drop dead servers; check zookeeper ideal state and external view; controller; 
+#restart server: 
+querying returning warning aboout unavailable segments. 
+It should have lines with Adding segment and Finished Adding Segment
+Also, a lot of performance info can be found in rocksdb logs itself. They are dumped every 5 minutes by default.
+for those you need to check; dataDir/tableName/rocksDB/LOG 
+Async profiler captures cpu time by default. For RocksDB, it is better if we capture wall time to account for IO waits as well
+
+# generate json files randomized; 
+./generateJsonSimple.py 
 
 #create segments
-/Users/zhenyunzhuang/workspace/startree-pinot/startree-distribution/target/startree-distribution-pkg/bin/pinot-admin.sh CreateSegment -dataDir /Users/zhenyunzhuang/workspace/z-tests/pinotUpsert/localMac/rawDataSimple -outDir /Users/zhenyunzhuang/workspace/z-tests/pinotUpsert/localMac/outputSimple -tableConfigFile /Users/zhenyunzhuang/workspace/z-tests/pinotUpsert/localMac/simpleMeetup_realtime_table_config.json -schemaFile /Users/zhenyunzhuang/workspace/z-tests/pinotUpsert/localMac/simpleMeetup_schema.json -overwrite -format JSON
+/Users/zhenyunzhuang/workspace/startree-pinot/startree-distribution/target/startree-pinot-0.13.0-SNAPSHOT-bin/startree-pinot-0.13.0-SNAPSHOT-bin/bin/pinot-admin.sh CreateSegment -dataDir /Users/zhenyunzhuang/workspace/z-tests/pinotUpsert/localMac/rawDataSimple -outDir /Users/zhenyunzhuang/workspace/z-tests/pinotUpsert/localMac/outputSimple -tableConfigFile /Users/zhenyunzhuang/workspace/z-tests/pinotUpsert/localMac/simpleMeetup_realtime_table_config.json -schemaFile /Users/zhenyunzhuang/workspace/z-tests/pinotUpsert/localMac/simpleMeetup_schema.json -overwrite -format JSON
 
 # Upload segments: 
-/Users/zhenyunzhuang/workspace/startree-pinot/startree-distribution/target/startree-distribution-pkg/bin/pinot-admin.sh UploadSegment -controllerHost localhost -controllerPort 9000 -tableName simpleMeetup -tableType REALTIME -segmentDir /Users/zhenyunzhuang/workspace/z-tests/pinotUpsert/localMac/outputSimple
+/Users/zhenyunzhuang/workspace/startree-pinot/startree-distribution/target/startree-pinot-0.13.0-SNAPSHOT-bin/startree-pinot-0.13.0-SNAPSHOT-bin/bin/pinot-admin.sh UploadSegment -controllerHost localhost -controllerPort 9000 -tableName simpleMeetup -tableType REALTIME -segmentDir /Users/zhenyunzhuang/workspace/z-tests/pinotUpsert/localMac/outputSimple
 
 #sql query
 select event_id, count(*) from simpleMeetup group by event_id order by count(*) desc limit 10
---
+++ generateJsonSimple.py
@@ -0,0 +1,40 @@
+#!/opt/homebrew/bin/python3
+
+import random
+
+numKRows = 1*1000  # how many K rows
+upsertPercent = 50 # how many % of the rows are upserting to old rows with same event id ; 
+
+inputFile = "rawdata.simple"
+if numKRows < 1000:
+    outputFile = str(numKRows)+"k.json"
+else:
+    outputFile = str(numKRows/1000)+"m.json"
+
+    
+
+
+# Open the input file for reading
+with open(inputFile, "r") as input_file:
+    # Read the entire file into a string variable
+    input_text = input_file.read()
+
+#{"event":{"event_name":"event_name-1062057629"},"group":{"group_topics":[{"topic_name":"topic_name5","urlkey":"http://group-url-711"}],"group_city":"group_city790","group_country":"group_country59","group_id":1931714650437847701,"group_name":"group_name26857140","group_lat":-42.04735223251407,"group_lon":18.51252826737668},"venue":{"venue_name":"venue_name1463550501"},"venue_name":"venue_name1463550501","event_name":"event_name-1062057629","event_id":"key_event_id","event_time":"2023-03-29 18:45:26.394225","group_city":"group_city790","group_country":"group_country59","group_id":1931714650437847701,"group_name":"group_name26857140","group_lat":-42.04735223251407,"group_lon":18.51252826737668,"mtime":1678416326394,"rsvp_id":10,"guests":79,"rsvp_count":10}
+# replace : key_event_id to a random number between 1 
+# 
+
+keyStr1 = "key_event_id"
+keyStr2 = "keymtime"
+maxNum = numKRows * 1000 / 100 * (100 - upsertPercent) +1  
+
+# Open the output file for writing
+with open(outputFile, "w") as output_file:
+    for row in range(numKRows*1000):
+        output_text = input_text.replace(keyStr1, "event_id_" + str(random.randint(0, maxNum)))
+        strDate = str(random.randint(100, 999)) 
+        output_text = output_text.replace(keyStr2, strDate)
+
+        # Write the modified text to the output file
+        output_file.write(output_text)
+
+
